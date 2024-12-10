a34834003dab87c8c6a11160d526b2f1aecb2e7f
Leonardo Santos <leonardodossantoss50@gmail.com>
2024-12-10 00:28:24 -0300

    Endpoints para geração de textos com API da Groq criada e testada. O usuário requisitante deverá ter uma conta criada em www.estampaverso.shop.

--- main.py
+++ main.py
@@ -1,25 +1,74 @@
+import os
+import json
+import logging
+import requests
+from datetime import datetime
+
 from groq import Groq
 
-import os
-from dotenv import load_dotenv
-load_dotenv()
+from typing import Union
+from fastapi import APIRouter, FastAPI
+from fastapi import HTTPException, status
+from pydantic import BaseModel
 
-client = Groq(
-    api_key=os.getenv('GROQ_API_KEY_1'),
-)
+logging.basicConfig(level=logging.INFO, filename="logs.log")
+logger = logging.getLogger(__name__)
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": """
-            Escreva um texto em português do Brasil, sobre:
-            A importância das IAs na criação de conteúdos para Posts e Redes Sociais.
-            """,
-        }
-    ],
-    model="llama3-8b-8192",
-    stream=False,
-)
+BASE_URL_API_DJANGO = os.getenv("BASE_URL_API_DJANGO_STAGING" if os.getenv("DEBUG") != "True" else "BASE_URL_API_DJANGO_PROD")
 
-print(chat_completion.choices[0].message.content)
+class Item(BaseModel):
+    username: str
+    password: str
+    context: str | None = None
+
+
+app = FastAPI()
+router = APIRouter()
+
+
+def api_v1_check_user(username:str, password:str):
+    url = f"{BASE_URL_API_DJANGO}/api/v1/check-user/{username}/{password}"
+    try:
+        return requests.get(
+            url=url
+            ).json()
+    except Exception as e:
+        print(e)
+        return None
+    
+
+@router.post("/api/groq/v1/generate-post-text-with-groq-IA/", summary="Gerar conteúdo com IA", description="Gera um texto baseado no contexto usando IA.")
+def api_v1_generate_post_text_with_groq_IA(item: Item):
+
+    dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    user = api_v1_check_user(username=item.username, password=item.password)
+    if not user or user.get("message") != "is_valid":
+        logger.error(f"ERROR CHECKUSER: {dt} | {user} ")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Credenciais inválidas ou erro ao verificar usuário."
+        )
+        
+    data = {"role": "user", "content": f"{item.context.strip()}"}
+    client = Groq(api_key=os.getenv('GROQ_API_KEY_1'))
+    try:
+        chat_completion = client.chat.completions.create(
+            messages=[data],
+            model=os.getenv("GROP_API_MODEL"),
+            stream=False,
+        )
+        logger.info(f"create_text_to: {dt} | {item.username}")
+        return {"data": {
+            "statusCode": 200,
+            "content": chat_completion.choices[0].message.content
+        }}
+    except Exception as e:
+        logger.error(f"Error: {dt} | {e}")
+        return {"data": {
+            "statusCode": 400,
+            "content": "",
+            "msg": "Erro ao gerar conteúdo! Verifique suas credenciais www.estampaverso.shop ou o conteúdo inserido."
+        }}
+
+
+app.include_router(router)
